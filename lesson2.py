2690f05caf17057c0e879aa53df3151339aa0ab9
alexsei_popov <popov2895@gmail.com>
2022-12-27 17:06:11 +0300

    add function

--- lesson2.py
+++ lesson2.py
@@ -1,3 +1,13 @@
 # Здесь бует урок 2
 # вот вот уже будет тут код
 
+n = 5
+
+h = [20, "hrkl", True]
+
+def name_of_func(a, b):
+    c = a + b
+    return c
+
+k = name_of_func(4, 6) * name_of_func(1, 2)
+print(k)
