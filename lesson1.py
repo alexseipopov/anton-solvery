ce2050bac268591e4735e89d56b642a6c373242c
alexsei_popov <popov2895@gmail.com>
2022-12-27 16:10:18 +0300

    add 2 files to tracking

---
+++ lesson1.py
@@ -0,0 +1,42 @@
+string = "my friend 'tony'"
+integer = 0
+float_ = 10.42468720357
+boolean = False
+
+# True - 1 ...
+# False - 0
+
+# pr = bool(integer)
+# print(type(pr), pr)
+
+# Collections
+
+# List (Список)
+#        0       1      2     3    4             5
+list_ = [10, "string", True, 10, 12.4, [12, False, [10, 8]]]
+# list_.append({
+#     "key1": 10
+# })
+# print(list_)
+
+# Tuple (Кортеж)
+#         0        1          2        3        4        5
+tuple_ = (20, "string too", False, (10, 9), [10, True], 20)
+# i = tuple_.count(20)
+# print(len(tuple_))
+
+# Dictionary (dict) - Словарь
+dict_ = {
+    "key": "value",
+    "number": 40,
+    "bool": True,
+    True: "some text",
+    (10, True): 13.5
+}
+# dict_.setdefault("key2", "val")
+
+# print(dict_.)
+
+# Set (Множество)
+set_ = {10, 30, True, 20.5, "str", 30}
+# print(set_)
