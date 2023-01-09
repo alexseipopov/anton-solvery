998d4e4cc61e4a0fa8e86fe259fa540cd15b9a7f
Алексей Попов <alexseipopov@MacBook-Pro-Alexsei.local>
2023-01-09 20:02:54 +0300

    lesson 3 added

---
+++ lesson3.py
@@ -0,0 +1,33 @@
+
+# Таблицы истинности
+#  True      False
+
+# if a == 0:
+#     print("0")
+# elif a == 1:
+#     print("1")
+# elif a == 2:
+#     print("2")
+# elif a == 3:
+#     print("3")
+# elif a == 4:
+#     print("4")
+# elif a == 3:
+#     print("дважды проверили")
+# else:
+#     print("не попадаем")
+
+# print("finish")
+
+a = 3
+
+
+# while a >= 10:
+#     print("do it")
+#     a = a - 1
+
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
